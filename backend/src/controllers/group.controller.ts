147bf15e2f2473ffe238b78c5aea2581e9aebe10
Test <adrienlegeleu@gmail.com>
2024-08-15 13:18:20 +0200

    auth presque finish

--- backend/src/controllers/group.controller.ts
+++ backend/src/controllers/group.controller.ts
@@ -2,6 +2,24 @@ import { type Response, type Request } from "express";
 import { GroupModel } from "../model/group.model";
 
 export class GroupController {
+  async findAll(req: Request, res: Response): Promise<void> {
+    try {
+      const group = await GroupModel.find();
+      if (!group) {
+        res.status(404).send({
+          error: "group not found",
+        });
+
+        return;
+      }
+      res.status(200).send(group);
+    } catch (err: any) {
+      console.log(err);
+      res.status(500).send({
+        error: err?.message,
+      });
+    }
+  }
   async findOneById(req: Request, res: Response): Promise<void> {
     try {
       const { groupId } = req.params;
