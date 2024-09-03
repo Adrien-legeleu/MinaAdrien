1f3e085be4e6ee01249744ee55cc88634a5b2b74
Test <adrienlegeleu@gmail.com>
2024-09-03 20:33:22 +0200

    lovnia corection

---
+++ .history/frontend/src/components/icons/index_20240708100744.ts
@@ -0,0 +1,11 @@
+export * from "./ArrowRight";
+export * from "./IconPeopleGroup";
+export * from "./IconPlusGroup";
+export * from "./IconNewGroup";
+export * from "./IconJoinGroup";
+
+export * from "./IconClose";
+export * from "./IconSetting";
+export * from "./IconGroup";
+export * from "./IconDelete";
+export * from "./IconUpdate";
