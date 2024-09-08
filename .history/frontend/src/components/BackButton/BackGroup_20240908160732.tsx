3060f406fe675c4013c18c3ecf08483664764954
Test <adrienlegeleu@gmail.com>
2024-09-08 16:08:59 +0200

    lovna

--- .history/frontend/src/components/BackButton/BackGroup_20240908160732.tsx
+++ .history/frontend/src/components/BackButton/BackGroup_20240908160732.tsx
@@ -7,7 +7,7 @@ export const BackGroup = () => {
     typeof window !== "undefined" ? localStorage.removeItem("groupId") : null;
   };
   return (
-    <Link href="/group" onClick={backGroup}>
+    <Link href="/group">
       <button className="rotate-180 py-2 px-4 max-sm:px-3 flex items-center justify-center max-sm:h-8 max-sm:w-12 rounded-full text-black bg-gray-100 cursor-pointer hover:-translate-x-1 duration-200 ease-in-out">
         <ArrowRight />
       </button>
--- .history/frontend/src/components/BackButton/BackHome_20240908160817.tsx
+++ .history/frontend/src/components/BackButton/BackHome_20240908160817.tsx
@@ -5,7 +5,8 @@ import { useGroupContext } from "@/context/GroupContexts";
 export const BackHome = () => {
   const { group } = useGroupContext();
   return (
-    <Link href={`/home/${group?._id}`} onClick={() => console.log(group)}>
+    <Link href={`/home/${group?._id}`} onClick={()=>console.log(group);
+    }>
       <button className="rotate-180 py-2 px-4 max-sm:px-3 max-sm:h-8 max-sm:w-12 flex items-center justify-center rounded-full text-black bg-gray-300 cursor-pointer">
         <ArrowRight />
       </button>
