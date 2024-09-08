da6e7dd5b892fe88f4aec7637243b90f87ac4d74
Test <adrienlegeleu@gmail.com>
2024-09-08 15:35:39 +0200

    lovna

--- .history/frontend/src/components/icons/IconNotifications_20240908153457.tsx
+++ .history/frontend/src/components/icons/IconNotifications_20240908153457.tsx
@@ -1,6 +1,6 @@
 export const IconNotifications = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       {" "}
       <path
         stroke="currentColor"
