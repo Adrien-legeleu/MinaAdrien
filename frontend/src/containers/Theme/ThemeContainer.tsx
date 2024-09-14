2dddae01bc5b233526f5df938da6fa44955e6b08
Test <adrienlegeleu@gmail.com>
2024-09-14 10:14:45 +0200

    lovna

--- frontend/src/containers/Theme/ThemeContainer.tsx
+++ frontend/src/containers/Theme/ThemeContainer.tsx
@@ -200,7 +200,7 @@ export const ThemeContainer: React.FC<ThemeContainerDetailsProps> = ({
                       {img.photoDate ? img.photoDate : "../../.."}
                     </div>
                     <div
-                      className="px-4 py-2 h-10 w-10 hover:scale-110 duration-300 ease-in-out cursor-pointer"
+                      className="px-4 py-2 h-20 flex items-center justify-center w-20 hover:scale-110 duration-300 ease-in-out cursor-pointer"
                       onClick={() => themeModalImgOpen(img._id)}
                     >
                       <IconLoop />
